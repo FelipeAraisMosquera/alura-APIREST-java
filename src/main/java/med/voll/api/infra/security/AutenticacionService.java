b431047da18d338b02022f6fb694dd032e7e1b0a
Felipe Arias Mosquera <felipearias.inge@gmail.com>
2023-09-19 00:00:54 -0500

    se añade la configuracion de spring security para que no aparezca el login por defecto y funcione correctamente ese dato es ((SessionCreationPolicy.STATELESS))

---
+++ src/main/java/med/voll/api/infra/security/AutenticacionService.java
@@ -0,0 +1,18 @@
+package med.voll.api.infra.security;
+
+import med.voll.api.domain.usuarios.UsuariosRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticacionService implements UserDetailsService {
+
+    private UsuariosRepository usuariosRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuariosRepository.findByLogin(username);
+    }
+}
